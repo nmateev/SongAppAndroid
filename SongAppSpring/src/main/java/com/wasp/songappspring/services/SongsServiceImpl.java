46d284a4b61bce1c5492cf0bbc412264da1a89e4
Nikolay Mateev <nmateev@abv.bg>
2018-09-04 15:21:34 +0300

    Added controller and service

---
+++ SongAppSpring/src/main/java/com/wasp/songappspring/services/SongsServiceImpl.java
@@ -0,0 +1,43 @@
+package com.wasp.songappspring.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SongsServiceImpl implements SongsServiceBase {
+    private SongsRepository songsRepository;
+
+    public SongsServiceImpl(SongsRepository songsRepository) {
+        setSongsRepository(songsRepository);
+    }
+
+    @Override
+    public void addSong(Song newSong) {
+        songsRepository.addSong(newSong);
+    }
+
+    @Override
+    public void deleteSong(int id) {
+        songsRepository.deleteSong(id);
+    }
+
+    @Override
+    public void updateSongPlayCounter(int id) {
+        songsRepository.updateSongPlayCounter(id);
+    }
+
+    @Override
+    public Song getSongById(int id) {
+        return songsRepository.getSongById(id);
+    }
+
+    @Override
+    public List<Song> getSongs() {
+        return songsRepository.getSongs();
+    }
+
+    private void setSongsRepository(SongsRepository songsRepository) {
+        this.songsRepository = songsRepository;
+    }
+}
