0751c62367f46e8e2dc1c26c8e537fb196681b04
Vinchenzo <vinchenco10@abv.bg>
2018-09-04 18:27:51 +0300

    add models

--- SongAppSpring/src/main/java/com/wasp/songappspring/repositories/InMemoryRepository.java
+++ SongAppSpring/src/main/java/com/wasp/songappspring/repositories/InMemoryRepository.java
@@ -19,8 +19,14 @@ public class InMemoryRepository implements SongsRepository {
     public InMemoryRepository(){
         idGenerator=new IdGenerator();
         songsList= new ArrayList<>();
-        songsList.add(new Song(idGenerator.getNextId(),"Detelini","Lili Ivanova","3.30",0,""));
-        songsList.add(new Song(idGenerator.getNextId(),"Edin nerazdelen klas","Tonika","3.54",0,""));
+        songsList.add(new Song(idGenerator.getNextId(),"Eastside","Benny Blanco, Hasley & Khalid","2.53",0,""));
+        songsList.add(new Song(idGenerator.getNextId(),"Promises","Calvin Harris, Sam Smith","4.05",0,""));
+        songsList.add(new Song(idGenerator.getNextId(),"Shotgun","George Ezra","3.27",0,""));
+        songsList.add(new Song(idGenerator.getNextId(),"In my feelings","Drake","7.59",0,
+                "https://www.google.bg/search?biw=1536&bih=759&tbm=isch&sa=1&ei=nKKOW8riEcL8sQHojIeIAw&q=drake&oq=drake&gs_l=img.3..0l10.8664.9262.0.9522.5.5.0.0.0.0.114.469.4j1.5.0....0...1c.1.64.img..0.5.468...35i39k1j0i67k1j0i10k1.0.7PQvPaBvFUs#imgrc=OVd5gRxKTtUTSM:"));
+        songsList.add(new Song(idGenerator.getNextId(),"Girls like you","Maroon 5","4.30",0,""));
+        songsList.add(new Song(idGenerator.getNextId(),"No brainer","Justin Bieber,Chance the rapper & Quavo","4.22",0,""));
+        songsList.add(new Song(idGenerator.getNextId(),"God is a woman","Ariana Grande","4.01",0,""));
     }
 
 
