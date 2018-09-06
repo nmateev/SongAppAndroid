22e30ad66d6299f28f5d3add198a699f8572e253
Vinchenzo <vinchenco10@abv.bg>
2018-09-07 02:07:19 +0300

    add (create) songs to InMemoryRepository

--- SongApp/app/src/main/java/com/wasp/songapp/utils/Constants.java
+++ SongApp/app/src/main/java/com/wasp/songapp/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.wasp.songapp.utils;
 
 public class Constants {
-    public static final String BASE_SERVER_URL = "http://192.168.1.77:9396/api/songs";
+    public static final String BASE_SERVER_URL = "http://192.168.0.1/api/songs";
     public static final String ERROR_MESSAGE = "Error: ";
 }
