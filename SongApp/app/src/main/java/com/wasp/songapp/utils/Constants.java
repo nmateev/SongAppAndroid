6dc1330a499d0f8b20e191d747e713d97b391168
Nikolay Mateev <nmateev@abv.bg>
2018-09-07 15:48:36 +0300

    Cleanup

--- SongApp/app/src/main/java/com/wasp/songapp/utils/Constants.java
+++ SongApp/app/src/main/java/com/wasp/songapp/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.wasp.songapp.utils;
 
 public class Constants {
-    public static final String BASE_SERVER_URL = "http://192.168.0.1/api/songs";
+    public static final String BASE_SERVER_URL = "http://192.168.1.77:9396/api/songs";
     public static final String ERROR_MESSAGE = "Error: ";
     public static final String NO_SONGS_AVAILABLE_MESSAGE = "Your song list is empty";
     public static final String NO_SONGS_FOUND_ON_SEARCH_MESSAGE = "No songs match your search";
